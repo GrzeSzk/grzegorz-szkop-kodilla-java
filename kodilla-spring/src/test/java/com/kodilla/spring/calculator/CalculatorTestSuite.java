cd9874957ff574787d6b9ab5831f617d992a0e90
Kreatorzy Wnętrz <KREATORZYWNETRZ@MacBook-Pro.local>
2022-05-09 19:29:00 +0200

    14.2 Calculator

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Autowired
+    Display display;
+
+    @Test
+    void testCalculations() {
+
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double addResult = calculator.add(2, 2);
+        double subResult = calculator.sub(4, 2);
+        double mulResult = calculator.mul(2, 2);
+        double divResult = calculator.div(4, 2);
+
+        //Then
+        assertEquals(4, addResult);
+        assertEquals(2, subResult);
+        assertEquals(4, mulResult);
+        assertEquals(2, divResult);
+    }
+}
