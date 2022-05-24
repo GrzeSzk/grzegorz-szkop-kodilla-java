b241760a38c43db271f7b223b6a4f23d23037741
Kreatorzy Wnętrz <KREATORZYWNETRZ@MacBook-Pro.local>
2022-05-24 09:00:44 +0200

    17.4 for corrections

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -6,6 +6,11 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@NamedNativeQuery(
+        name = "Company.findCompanyByParameter",
+        query = "SELECT * FROM COMPANIES WHERE COMPANY_NAME LIKE :ARG",
+        resultClass = Company.class
+)
 @Entity
 @Table(name = "COMPANIES")
 public class Company {
